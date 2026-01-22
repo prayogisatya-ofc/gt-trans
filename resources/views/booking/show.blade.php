04c52006d15c4b47917d0a2ce6dd2062f548438e
prayogisatya-ofc <besutodio@gmail.com>
2026-01-22 18:34:20 +0700

    fix: correct service type condition in booking details view

--- resources/views/booking/show.blade.php
+++ resources/views/booking/show.blade.php
@@ -51,7 +51,7 @@
                         <svg class="w-3.5 h-3.5 sm:w-4 sm:h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"/>
                         </svg>
-                        {{ $booking->service_type === 'regular' ? 'Reguler (Umum)' : ($service_type === 'charter' ? 'Carter (Private)' : 'Paket Kilat') }}
+                        {{ $booking->service_type === 'regular' ? 'Reguler (Umum)' : ($booking->service_type === 'charter' ? 'Carter (Private)' : 'Paket Kilat') }}
                     </span>
                 </div>
                 
